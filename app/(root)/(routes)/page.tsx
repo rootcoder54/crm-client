12c537b05a8bdf9ce7b62c921cc81d6aeb69c1d8
ozymandias54 <bassirou718@gmail.com>
2025-10-09 15:19:18 +0000

    page home requete en cours

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,108 +1,75 @@
 "use client";
-import Image from "next/image";
-import { SidebarTrigger } from "@/components/ui/sidebar";
-import { useIsMobile } from "@/hooks/use-mobile";
+
+import HeaderPage from "@/components/features/header-page";
+import { Mode } from "@/components/features/theme";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from "@/components/ui/table";
+import { fetcher } from "@/lib/fetcher";
+import { cn } from "@/lib/utils";
+import { Requete } from "@prisma/client";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
-  const ismobile = useIsMobile();
-  return (
-    <div className="flex flex-col items-start justify-items-center min-h-screen pb-20 gap-16">
-      <main className="flex flex-col gap-[32px] row-start-2 items-start sm:items-start p-5">
-        {ismobile && <SidebarTrigger className="-ml-1" />}
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const { data: requetes } = useQuery<Requete[]>({
+    queryKey: ["requete"],
+    queryFn: () => fetcher(`/api/requete`)
+  });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <div>
+      <HeaderPage chemins={[{ title: "Home", url: "/" }]}>
+        <Mode />
+      </HeaderPage>
+      <div className="flex-1 h-full p-4">
+        <h1 className="text-xl">Page d&apos;accueil</h1>
+        <p>Bienvenue sur la page d&apos;accueil de notre application !</p>
+        <hr className="my-5" />
+        <h1 className="text-xl text-blue-500 font-bold">Requête en cours</h1>
+        <div className="flex flex-row items-center justify-center gap-4 mt-4">
+          <Table className="border-y w-full">
+            <TableHeader className="bg-zinc-600/10">
+              <TableRow className="w-full">
+                <TableHead>Sujet</TableHead>
+                <TableHead>Description</TableHead>
+                <TableHead>Type</TableHead>
+                <TableHead>Demadeur</TableHead>
+                <TableHead>Technicien</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {requetes && requetes.length > 0 ? (
+                requetes.map((requete) => (
+                  <TableRow
+                    key={requete.id}
+                    className={cn(
+                      "cursor-pointer w-full h-8",
+                      requete.dateCloture && "hidden"
+                    )}
+                  >
+                    <TableCell>{requete.sujet}</TableCell>
+                    <TableCell>{requete.description}</TableCell>
+                    <TableCell>{requete.type}</TableCell>
+                    <TableCell>{requete.demandeur}</TableCell>
+                    <TableCell>{requete.technicien}</TableCell>
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell colSpan={5} className="h-24 text-center">
+                    Pas de resultat
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
