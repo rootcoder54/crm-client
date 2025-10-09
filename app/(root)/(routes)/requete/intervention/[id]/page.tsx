95a4c7704308bb4dce6a9fe5cb11579a801e2ff4
ozymandias54 <bassirou718@gmail.com>
2025-10-09 16:37:44 +0000

    intervention update

--- app/(root)/(routes)/requete/intervention/[id]/page.tsx
+++ app/(root)/(routes)/requete/intervention/[id]/page.tsx
@@ -31,6 +31,45 @@ const InterventionRequetePage = async ({
   return (
     <div>
       <InterventionRequete requete={requete} items={intervention.items} />
+      <hr className="my-4" />
+      <div className="grid gap-4 py-4">
+        <div className="grid grid-cols-2 gap-4 px-10">
+          <div className="flex flex-col space-y-1.5">
+            <p className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+              Service
+            </p>
+            <p className="text-sm text-muted-foreground">
+              {intervention.service}
+            </p>
+          </div>
+          <div className="flex flex-col space-y-1.5">
+            <p className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+              Technicien
+            </p>
+            <p className="text-sm text-muted-foreground">
+              {intervention.intervenant}
+            </p>
+          </div>
+          <div className="flex flex-col space-y-1.5">
+            <p className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+              Observation
+            </p>
+            <p className="text-sm text-muted-foreground">
+              {intervention.observations
+                ? intervention.observations
+                : "Aucune observation"}
+            </p>
+          </div>
+          <div className="flex flex-col space-y-1.5">
+            <p className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+              Nature
+            </p>
+            <p className="text-sm text-muted-foreground">
+              {intervention.nature?.toUpperCase()}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
