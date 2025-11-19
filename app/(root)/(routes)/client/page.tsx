ce094230d69f5275096c1b4eb4dff16b89f715ae
ozymandias54 <bassirou718@gmail.com>
2025-11-19 18:11:09 +0000

    fix 20251119_1810

--- app/(root)/(routes)/client/page.tsx
+++ app/(root)/(routes)/client/page.tsx
@@ -6,8 +6,6 @@ import { LoaderOne } from "@/components/ui/loader";
 import { fetcher } from "@/lib/fetcher";
 import { Client } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
-import { format } from "date-fns";
-import { fr } from "date-fns/locale";
 import {
   AlertCircleIcon,
   FileChartPie,
@@ -61,25 +59,6 @@ const ClientPage = () => {
     );
   }
 
-  const listes = clients
-    ?.sort((a, b) => {
-      if (!a.dateLastVisite) return 1;
-      if (!b.dateLastVisite) return -1;
-      const dateA = new Date(a.dateLastVisite);
-      const dateB = new Date(b.dateLastVisite);
-      return dateB.getTime() - dateA.getTime(); // plus récent en premier
-    })
-    .map((client) => ({
-      ...client,
-      dateRequete: client.dateLastVisite
-        ? format(new Date(client.dateLastVisite), "dd MMM yyyy", {
-            locale: fr
-          })
-        : null,
-      numero: client.numero,
-      activite: client.activite || "Pas d'activité"
-    }));
-
   return (
     <DataTable
       chemins={[
@@ -116,7 +95,7 @@ const ClientPage = () => {
           variantbtn: "danger"
         }
       ]}
-      data={listes || []}
+      data={clients || []}
       hideList={[
         "createdAt",
         "updatedAt",
--- app/api/client/route.ts
+++ app/api/client/route.ts
@@ -1,4 +1,6 @@
 import { getAllClients } from "@/services/client.service";
+import { format } from "date-fns";
+import { fr } from "date-fns/locale";
 import { NextResponse } from "next/server";
 
 
@@ -6,7 +8,26 @@ export async function GET() {
   try {
     const clients = await getAllClients();
 
-    return NextResponse.json(clients);
+    const listes = clients
+        ?.sort((a, b) => {
+          if (!a.dateLastVisite) return 1;
+          if (!b.dateLastVisite) return -1;
+          const dateA = new Date(a.dateLastVisite);
+          const dateB = new Date(b.dateLastVisite);
+          return dateB.getTime() - dateA.getTime(); // plus récent en premier
+        })
+        .map((client) => ({
+          ...client,
+          dateRequete: client.dateLastVisite
+            ? format(new Date(client.dateLastVisite), "dd MMM yyyy", {
+                locale: fr
+              })
+            : null,
+          numero: client.numero,
+          activite: client.activite || "Pas d'activité"
+        }));
+
+    return NextResponse.json(listes);
   } catch (error) {
     return new NextResponse(`Internal Error :${error}`, { status: 500 });
   }
