a8d6fc2f0106a03d7d7e80a57504ba6428ba3866
ozymandias54 <bassirou718@gmail.com>
2026-09-14 11:56:16 +0000

    securisé route api

--- constante/routes.ts
+++ constante/routes.ts
@@ -3,7 +3,7 @@
  * These routes do not require authentication
  * @type {string[]}
  */
-export const publicRoutes = ["/support", "/formation", "/telechargement"];
+export const publicRoutes = ["/support", "/formation", "/telechargement","/api/video","/api/article"];
 
 /**
  * An array of routes that are used for authentication
@@ -17,7 +17,7 @@ export const authRoutes = ["/login"];
  * Routes that start with this prefix are used for API authentication purposes
  * @type {string}
  */
-export const apiAuthPrefix = "/api/";
+export const apiAuthPrefix = "/api/auth";
 
 /**
  * The default redirect path after logging in
